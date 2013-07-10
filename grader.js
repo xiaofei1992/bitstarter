41ca546c84320bd3a1264b90b89e0e801cb4d6c9
Xiaofei Cheng <xc76@georgetown.edu>
2013-07-10 17:21:03 +0000

    Updated

--- grader.js
+++ grader.js
@@ -2,11 +2,10 @@
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
+and basic DOM parsing. 
 
 References:
-
- + cheerio
++ cheerio
    - https://github.com/MatthewMueller/cheerio
    - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
    - http://maxogden.com/scraping-with-node.html
@@ -22,8 +21,10 @@ References:
 */
 
 var fs = require('fs');
+var util = require('util');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -55,6 +56,26 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+function buildFnc(checkFile) {
+  var urlCallback = function(result, response) {
+    if (result instanceof Error) {
+      console.log('Error: ' + util.format(result.message));
+    } else {
+        $ = cheerio.load(result);
+        var checks = loadChecks(checkFile).sort(); 
+        var out = {};
+        for(var ii in checks) {
+          var present = $(checks[ii]).length > 0;
+          out[checks[ii]] = present;
+        }
+        var outJson = JSON.stringify(out, null, 4);
+        console.log(outJson);
+    }
+  }
+  return urlCallback;
+};
+
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -63,12 +84,19 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <url>', 'Http URL link')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    
+    if (program.url) {
+	var cb = buildFnc(program.checks);
+	rest.get(program.url).on('complete', cb);
+	} else {
+	    var checkJson = checkHtmlFile(program.file, program.checks);
+	    var outJson = JSON.stringify(checkJson, null, 4);
+	    console.log(outJson);
+	    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
