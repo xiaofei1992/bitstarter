a58888ae5473332d64f5c750c43d9a8d46fb04c6
xiaofei1992 <cheng@gmail.com>
2013-08-12 19:05:30 +0000

    updated

--- web.js
+++ web.js
@@ -7,6 +7,13 @@ app.get('/', function(request, response) {
     response.send(indexContents.toString());
 });
 
+fs.readFile('contact.html', function (err, data) { if (err) throw err;
+data2=data.toString(); return data2;});
+
+app.get('/contact', function(request, response) { response.send(data2);});
+
+
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
